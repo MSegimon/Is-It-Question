261b2a0bcb3754ed982c694d67ba4d6174c90b41
Manuel Segimón <manuelsegimon68@gmail.com>
2021-08-04 20:10:07 +0200

    Add disconnect because I forgot to before

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ import core
 core.connect()
 
 # Main code
+<<<<<<< Updated upstream
 response = core.run_query("SELECT * FROM chatbot")
 print(response)
+=======
+
+
+
+
+# Disconnect from the server
+core.disconnect()
+>>>>>>> Stashed changes
