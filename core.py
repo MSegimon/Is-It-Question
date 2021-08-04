59dab2d35a6d209863c266b381db7e70bb9ddb88
Manuel Segimón <manuelsegimon68@gmail.com>
2021-08-04 14:08:58 +0200

    Established connection
    Connection was established through an ssh tunnel. The file with the server credentials was ignored.

---
+++ core.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import pymysql
+import logging
+import sshtunnel
+from sshtunnel import SSHTunnelForwarder
+
+# Credentials
+import serverCredentials
+
+
+# Open Tunnel
+def open_ssh_tunnel(verbose=False):
+    """Open an SSH tunnel and connect using a username and password.
+    
+    :param verbose: Set to True to show logging
+    :return tunnel: Global SSH tunnel connection
+    """
+
+    if verbose:
+        sshtunnel.DEFAULT_LOGLEVEL = logging.DEBUG
+
+    global tunnel
+    tunnel = SSHTunnelForwarder(
+        (serverCredentials.ssh_host, 22),
+        ssh_username=serverCredentials.ssh_username,
+        ssh_password=serverCredentials.ssh_password,
+        remote_bind_address=('127.0.0.1', 3306)
+    )
+
+    tunnel.start()
+
+# Connect to MySQL via the SSH tunnel
+def mysql_connect():
+    """Connect to a MySQL server using the SSH tunnel connection
+    
+    :return connection: Global MySQL database connection
+    """
+
+    global connection
+
+    connection = pymysql.connect(
+        host='127.0.0.1',
+        user=serverCredentials.database_username,
+        passwd=serverCredentials.database_password,
+        db=serverCredentials.database_name,
+        port=tunnel.local_bind_port
+    )
+
+# Run SQL query
+def run_query(sql):
+    """Runs a given SQL query via the global database connection.
+    
+    :param sql: MySQL query
+    :return: Pandas dataframe containing results
+    """
+
+    return pd.read_sql_query(sql, connection)
+
+
+# Disconnect and close the tunnel
+def mysql_disconnect():
+    """Closes the MySQL database connection.
+    """
+
+    connection.close()
+
+def close_ssh_tunnel():
+    """Closes the SSH tunnel connection.
+    """
+
+    tunnel.close
+
+
+# Run code
+open_ssh_tunnel()
+mysql_connect()
+df = run_query("SELECT * FROM chatbot")
+df.head()
+print(df)
+
+mysql_disconnect()
+close_ssh_tunnel()
