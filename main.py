7a522da89e674ac8fb97cafa4f1b201a97a4d4ac
Manuel Segimón <manuelsegimon68@gmail.com>
2021-08-05 18:29:08 +0200

    get result from server

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ import cosineSimilarity
 core.connect()
 
 # Main code
+def isQuestion(text):
+    result = core.run_query("SELECT text FROM chatbot WHERE isQuestion = 1")
 
+    print(result[0])
+
+
+isQuestion('yeet')
 
 # Disconnect from server
 core.disconnect()
